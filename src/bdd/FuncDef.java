a2c626ef593f86dddb4d78d44ae18c39dcdbf805
Ingrid Fondja <Ingridornella14@yahoo.com>
2019-12-23 11:28:04 +0100

    update  Funcdef

--- src/bdd/FuncDef.java
+++ src/bdd/FuncDef.java
@@ -10,7 +10,7 @@ public class FuncDef {
 		this.conn = conn;	
 	}
 	
-	public void createTable() {
+/*	public void createTable() {
 		String sql = " CREATE TABLE IF NOT EXISTS FuncDep (\n"
 				+ "        table_name  text NOT NULL,\n"
 				+ "         lhs        text NOT NULL,\n"
@@ -26,20 +26,56 @@ public class FuncDef {
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
-	}
+	}*/
+	
 	public void addDf(String rel,String[]lhs,String rhs) {
 		String sql = "INSERT INTO FuncDep (table_name, lhs , rhs) VALUES(?,?,?)";
 		
 	}
 	
 	
-	public  boolean satisfaction(String table) {
+	public  boolean satisfaction(String table, String lhs,String rhs) {
 		
-		
-		
+		ArrayList<String> tab = new ArrayList<String>();
+		tab = conn.getNameAttributes(table);
+		String[] att = lhs.split(" ");
+		String ls ="SELECT ";
+		for(int i =0; i<att.length; i++) {
+			if(i!= att.length - 1)
+			ls += att[i]+",";
+		}
+		String sql1 = ls+" FROM "+table;
+		Statement stmt1 = null;
+		String sql2 = "SELECT "+rhs+" FROM "+table;
+		try {
+			stmt1 = conn.getCon().createStatement();
+			ResultSet result1 = stmt1.executeQuery(sql1);
+			ResultSetMetaData rsd1 = result1.getMetaData();
+			ResultSet result2 = stmt1.executeQuery(sql2);
+			ResultSetMetaData rsd2 = result2.getMetaData();
+			while(result1.next() && result2.next()) {
+				
+			}
+
+		} catch (SQLException e) {
 			
-		
+			e.printStackTrace();
+		}		
 		return true;
+	}
+	
+	public void removeDf(String table, String l,String r) {
+		
+		try {
+			String str = "DELETE FROM FuncDep WHERE (table_name = "+table+ " and lhs =" +l+ "and rhs ="+r+")";
+			Statement st= conn.getCon().createStatement();
+			ResultSet rslt = st.executeQuery(str);
+			
+		} catch (SQLException e) {
+		
+			e.printStackTrace();
+		}
+		
 	}
 	
 	public ArrayList<ArrayList<String>> ReadFuncdep(){
@@ -82,8 +118,28 @@ public class FuncDef {
 		return arr;
 		
 	}
-	 
-	
-	
+	public ArrayList<ArrayList<String>> Select(String a, String table){
+		ArrayList<ArrayList<String>> tuple = new ArrayList<>();
+		String sql = "SELECT "+a+" FROM "+table;
+		Statement stmt;
+		try {
+			stmt = conn.getCon().createStatement();
+			ResultSet result = stmt.executeQuery(sql);
+			ResultSetMetaData rs = result.getMetaData();
+			int nbcol = rs.getColumnCount();
+			while(result.next()) {
+			 ArrayList<String> a1 = new ArrayList<>();
+			 for (int i = 1; i<=nbcol; i++) {
+				 a1.add(result.getObject(i).toString());
+			 }
+			 tuple.add(a1);
+			 }
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return tuple;
+		}
+		
 }
 
